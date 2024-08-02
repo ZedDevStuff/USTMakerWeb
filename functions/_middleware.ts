24d08bfa7e84d5406de79676c89212a0267b56ab
ZedDevStuff <kouamejuniorauguste@gmail.com>
2024-08-02 11:47:56 +0000

    CORS function modification...

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,6 +1,20 @@
-export const onRequest: PagesFunction = async (context) => {
+// Respond to OPTIONS method
+export const onRequestOptions: PagesFunction = async () => {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': 'ustmakerweb.zeddevstuff.dev',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Access-Control-Max-Age': '86400',
+      },
+    });
+  };
+  
+  // Set CORS to all /api responses
+  export const onRequest: PagesFunction = async (context) => {
     const response = await context.next();
-    response.headers.set('Access-Control-Allow-Origin', 'https://ustmakerweb.zeddevstuff.dev');
+    response.headers.set('Access-Control-Allow-Origin', 'ustmakerweb.zeddevstuff.dev');
     response.headers.set('Access-Control-Max-Age', '86400');
     return response;
-};
+  };
